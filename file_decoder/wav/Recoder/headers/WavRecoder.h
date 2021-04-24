9196f1130add8b5a7bed5f360d1a7b0812750110
Alex375 <alexandre.josien@gmail.com>
2021-04-24 22:41:06 +0200

    missing file

--- file_decoder/wav/Recoder/headers/WavRecoder.h
+++ file_decoder/wav/Recoder/headers/WavRecoder.h
@@ -2,11 +2,11 @@
 // Created by Alexandre Josien on 31/03/2021.
 //
 
-#ifndef SOUNDHOOK_RECODER_H
-#define SOUNDHOOK_RECODER_H
+#ifndef SOUNDHOOK_WAVRECODER_H
+#define SOUNDHOOK_WAVRECODER_H
 
 #include <stdio.h>
 
 void recodeWav(int* data, FILE* or, long size);
 
-#endif //SOUNDHOOK_RECODER_H
+#endif //SOUNDHOOK_WAVRECODER_H
