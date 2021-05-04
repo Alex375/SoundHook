44ae12e90caf9eb08b6524539932543951969425
Alex375 <alexandre.josien@gmail.com>
2021-05-04 11:31:23 +0200

    added wavlet call

---
+++ wavlet/headers/Wavelet.h
@@ -0,0 +1,10 @@
+//
+// Created by Alexandre Josien on 04/05/2021.
+//
+
+#ifndef SOUNDHOOK_WAVELET_H
+#define SOUNDHOOK_WAVELET_H
+#include "../../file_decoder/wav/types/wav.h"
+void wavelet(WavData* wavData);
+
+#endif //SOUNDHOOK_WAVELET_H
